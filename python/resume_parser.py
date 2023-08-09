a07b037d1b9ba782da0ffd48e29aca803a2ff957
Vedant <kamblivedant50@gmal.com>
2023-08-10 01:06:11 +0530

    i forgor

--- python/resume_parser.py
+++ python/resume_parser.py
@@ -8,50 +8,54 @@ from PyPDF2 import PdfReader
 import openai
 
 #Converting resume from pdf or docx to text
-class Convert2Text:
-    def __init__(self, file_path):
-        self.file_path = file_path
+# class Convert2Text:
+#     def __init__(self, file_path):
+#         self.file_path = file_path
 
-    def check_file_extension(self):
-        _, file_extension = os.path.splitext(self.file_path)
-        return file_extension.lower()
 
-    def convert_to_text(self):
-        file_extension = self.check_file_extension()
-        if file_extension == '.pdf':
-            return self.pdftotext(self.file_path)
-        elif file_extension == '.docx':
-            return self.doctotext(self.file_path)
-        else:
-            raise ValueError(f"Unsupported file format: {file_extension}. Only .pdf and .docx files are supported.")
 
-    def doctotext(self, m):
-        temp = docx2txt.process(m)
-        resume_text = [line.replace('\t', ' ') for line in temp.split('\n') if line]
-        text = ' '.join(resume_text)
-        return text
+def check_file_extension(self):
+    _, file_extension = os.path.splitext(self.file_path)
+    return file_extension.lower()
 
-    def pdftotext(self, m):
-        pdfFileObj = open(m, 'rb')
-        pdfFileReader = PdfReader(pdfFileObj)
-        num_pages = len(pdfFileReader.pages)
-        currentPageNumber = 0
-        text = ''
-        while currentPageNumber < num_pages:
-            pdfPage = pdfFileReader.pages[currentPageNumber]
-            text = text + pdfPage.extract_text()
-            currentPageNumber += 1
-        pdfFileObj.close()
-        return text
+def convert_to_text(self):
+    file_extension = self.check_file_extension()
+    if file_extension == '.pdf':
+        return self.pdftotext(self.file_path)
+    elif file_extension == '.docx':
+        return self.doctotext(self.file_path)
+    else:
+        raise ValueError(f"Unsupported file format: {file_extension}. Only .pdf and .docx files are supported.")
 
-resume = 'GET FROM DATABASE'
+def doctotext(self, m):
+    temp = docx2txt.process(m)
+    resume_text = [line.replace('\t', ' ') for line in temp.split('\n') if line]
+    text = ' '.join(resume_text)
+    return text
 
-converter = Convert2Text()
+def pdftotext(self, m):
+    pdfFileObj = open(m, 'rb')
+    pdfFileReader = PdfReader(pdfFileObj)
+    num_pages = len(pdfFileReader.pages)
+    currentPageNumber = 0
+    text = ''
+    while currentPageNumber < num_pages:
+        pdfPage = pdfFileReader.pages[currentPageNumber]
+        text = text + pdfPage.extract_text()
+        currentPageNumber += 1
+    pdfFileObj.close()
+    return text
+
+
+
+resume = 'Hack_Resume_25_7_23.pdf'
+
+converter = Convert2Text(resume)
 
 resume_text = converter.convert_to_text()
 
 # Chatgpt model to extract skills
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("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")
 
 response = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
@@ -88,7 +92,7 @@ def cv_ranker(applicant_skills, required_skills):
     return final_rank
 
 fr = cv_ranker(applicant_skills, required_skills)
-
+print(fr)
 threshold = 8
 
 if (fr> threshold):
