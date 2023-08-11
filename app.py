792a5f16d5614df8bb4a6f6041a83d5ccf510327
Vedant <kamblivedant50@gmal.com>
2023-08-11 16:37:11 +0530

    modify for ec2

--- app.py
+++ app.py
@@ -361,4 +361,4 @@ def run_script():
         return jsonify({'error': 'Invalid file format'})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=8080)
