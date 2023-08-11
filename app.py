9083e57dba60394318cc688f41d4819607375764
Vedant <kamblivedant50@gmal.com>
2023-08-12 02:12:01 +0530

    About page

--- app.py
+++ app.py
@@ -359,4 +359,5 @@ def run_script():
         return jsonify({'error': 'Invalid file format'})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    # app.run(host='0.0.0.0', port=8080)
+    app.run(debug=True)
